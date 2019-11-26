9755baf2e35ae2135fa421a953716c8dcc574296
Sim <sim@snorl.ax>
2019-11-26 09:25:18 +0800

    Minify pages using htmlmin package from python (Cannot implement it very well using nuxt)

---
+++ htmlminify.py
@@ -0,0 +1,16 @@
+﻿import htmlmin
+import os
+
+def minify(path):
+    """write the minified html to the original file"""
+    with open(path, 'r') as page:
+        page_con = page.read()
+    print("Minifying %s" % path)
+    minified = htmlmin.minify(page_con)
+    with open(path, 'w') as writer:
+        writer.write(minified)
+
+for root, dirs, files in os.walk("dist"):
+    for thing in files:
+        if thing.endswith(".html"):
+            minify(os.path.join(root, thing))
