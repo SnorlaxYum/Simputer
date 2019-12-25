ce77c0b7bcbd1d2bf384440a66254bb2d25c1e5f
Sim <sim@snorl.ax>
2019-12-25 09:52:32 +0800

    even better, just reference that

---
+++ nuxt.config.ts
@@ -0,0 +1,76 @@
+import gen from './gen_list.json'
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  generate: {routes: gen, fallback: true},
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    // Load a Node.js module directly (here it's a Sass file)
+    // 'bulma',
+    // CSS file in the project
+    // '@/assets/css/main.css',
+    // SCSS file in the project
+    '@/assets/css/main.sass'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: ['@nuxt/typescript-build'],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    'cookie-universal-nuxt',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  hooks: {
+    build: {
+      generate: {
+        done(nuxt: any, errors: any) {
+        console.log(errors)
+      }
+    }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+   extractCSS: true
+  }
+}
