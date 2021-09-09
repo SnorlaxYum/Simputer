d41f72d8a8e215a58a768fc18e8d22937d285453
Sim <sim@snorl.ax>
2021-09-09 10:03:00 +0800

    now no need processing _blank logics on frontend

--- components/Post.vue
+++ components/Post.vue
@@ -34,56 +34,6 @@ export default {
     tags: {
       type: Array
     }
-  },
-  watch: {'content': 'contentUpdated'},
-  methods: {
-    navigate(event) {
-      let target = event.target
-      let i = 0
-
-      // go through 5 parents max to find a tag
-      while (i < 5 && !(target instanceof HTMLAnchorElement) && target.parentNode) {
-        target = target.parentNode
-        i++
-      }
-
-      // ignore if no link found
-      if (!(target instanceof HTMLAnchorElement)) { return }
-
-      const href = target.getAttribute('href')
-
-      // use router to push it if it's on this site
-      if (href && !(href.indexOf("png")+1) && !(href.indexOf("webp")+1) && href[0] === '/' || href[0] === '#') {
-        event.preventDefault()
-        this.$router.push(href)
-      } else {
-        event.preventDefault()
-        window.open(href, '_blank')
-      }
-    },
-    addListeners() {
-      this._links = this.$el.getElementsByTagName('a')
-      for (let link of this._links) {
-        link.addEventListener('click', this.navigate)
-      }
-    },
-    removeListeners() {
-      for (let link of this._links) {
-        link.removeEventListener('click', this.navigate)
-      }
-    },
-    contentUpdated() {
-      this.removeListeners()
-      this.$nextTick(()=>{
-        this.addListeners()
-      })
-    }
-  },
-  mounted() {
-    this.addListeners()
-  },
-  beforeDestroy() {
-    this.removeListeners()
   }
 }
 </script>
